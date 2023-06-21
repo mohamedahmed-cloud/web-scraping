0dc5ddac924a183e2157d067fd5b08796537711c
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-06-21 13:57:30 +0300

    Get contest Number and link from codeforces

--- GetRoundsFromCodeforces/app.py
+++ GetRoundsFromCodeforces/app.py
@@ -15,7 +15,7 @@ print("Number of contest is ",len(data))
 with open("result.csv", "w") as file:
     write = csv.writer(file)
     write.writerow([ "عليه أفضل الصلاة والسلام",'صلى على أفضل خلق الله'])
-    write.writerow("No.Of Div", "Div Link")
+    write.writerow(["No.Of Div", "Div Link"])
     write.writerows(data)
 
     
